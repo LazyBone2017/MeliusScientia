9b8cb2aa931c71dd05146c13c131f627cc100109
Moritz Vogel <moritzv7@googlemail.com>
2020-06-02 22:31:32 +0200

    feat: add generic recipe handling

---
+++ src/main/java/de/lazybird/meliusscientia/init/RecipeInit.java
@@ -0,0 +1,12 @@
+package de.lazybird.meliusscientia.init;
+
+import de.lazybird.meliusscientia.recipe.CrusherRecipeHandler;
+
+public class RecipeInit {
+
+    public static CrusherRecipeHandler crusherRecipeHandler = new CrusherRecipeHandler();
+
+    public static void registerRecipes() {
+        crusherRecipeHandler.addRecipe(ModBlock.uranium_ore.get().asItem(), ModItem.bottle_u03.get());
+    }
+}
